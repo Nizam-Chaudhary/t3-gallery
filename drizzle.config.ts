2550431f1b90c2787da714f4b29e5cba526e139d
Nizam Chaudhary <ChaudharyNizam10@gmail.com>
2025-11-30 23:43:12 +0530

    chore: rename to t3 gallery

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -8,5 +8,5 @@ export default {
 	dbCredentials: {
 		url: env.DATABASE_URL,
 	},
-	tablesFilter: ["next-gallery_*"],
+	tablesFilter: ["t3-gallery_*"],
 } satisfies Config;
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -9,7 +9,7 @@ import { index, pgTableCreator } from "drizzle-orm/pg-core";
  *
  * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
  */
-export const createTable = pgTableCreator((name) => `next-gallery_${name}`);
+export const createTable = pgTableCreator((name) => `t3-gallery_${name}`);
 
 export const posts = createTable(
 	"post",
